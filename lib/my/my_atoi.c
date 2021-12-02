51b491f3806cef88e07f18553e50d65a2dc33a49
clementloeuillet <clement.loeuillet@homail.com>
2021-12-02 09:10:30 +0100

    my_atoi >> skip no digit char

--- lib/my/my_atoi.c
+++ lib/my/my_atoi.c
@@ -12,6 +12,7 @@ int my_atoi(char *str)
     int res = 0;
 
     for (int i = 0; str[i] != '\0'; ++i)
-        res = res * 10 + str[i] - 48;
+        if (str[i] >= '0' && str[i] <= '9')
+            res = res * 10 + str[i] - 48;
     return res;
 }
